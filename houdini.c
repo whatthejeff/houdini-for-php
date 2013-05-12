43cb6d575ca8e1b40259d263a2577d274c30b43a
Jeff Welch <whatthejeff@gmail.com>
2013-05-12 10:19:40 -0400

    Cleanup in preparation for PECL submission.

--- houdini.c
+++ houdini.c
@@ -22,24 +22,28 @@
 
 #include "php.h"
 #include "ext/standard/info.h"
+#include "zend_exceptions.h"
 #include "houdini.h"
 #include "php_houdini.h"
-
-#include <zend_exceptions.h>
-
-static int le_houdini;
 
 static zend_class_entry *houdini_ce_HoudiniException;
 
 #define PHP_HOUDINI_CHECK_UTF8(string, length) \
-	if (!php_houdini_check_utf8_encoding(string, length)) { \
-		zend_throw_exception(houdini_ce_HoudiniException, "Input string must be valid UTF-8", 0 TSRMLS_CC); \
-		return; \
-	}
+	do { \
+		size_t position = 0; \
+		int status; \
+\
+		while (position < length) { \
+			php_houdini_next_utf8_char((const unsigned char *)string, length, &position, &status); \
+			if (status != SUCCESS) { \
+				zend_throw_exception(houdini_ce_HoudiniException, "Input string must be valid UTF-8", 0 TSRMLS_CC); \
+				return; \
+			} \
+		} \
+	} while(0)
 
 #define PHP_HOUDINI_FUNCTION(function_name) \
-	PHP_FUNCTION(function_name) \
-	{ \
+	do{ \
 		char *string = NULL; \
 		int length; \
 \
@@ -53,7 +57,7 @@ static zend_class_entry *houdini_ce_HoudiniException;
 			RETURN_EMPTY_STRING(); \
 		} \
 \
-		PHP_HOUDINI_CHECK_UTF8(string, length) \
+		PHP_HOUDINI_CHECK_UTF8(string, length); \
 \
 		if (function_name(&buffer, (const uint8_t *)string, length)) { \
 			RETVAL_STRINGL(buffer.ptr, buffer.size, 1); \
@@ -61,7 +65,7 @@ static zend_class_entry *houdini_ce_HoudiniException;
 		} else { \
 			RETVAL_STRINGL(string, length, 1); \
 		} \
-	}
+	} while (0)
 
 #define MB_FAILURE(pos, advance) do { \
 	*cursor = pos + (advance); \
@@ -162,35 +166,122 @@ static inline unsigned int php_houdini_next_utf8_char(
 	}
 
 	*cursor = pos;
-  	return this_char;
+	return this_char;
 }
 
-static int php_houdini_check_utf8_encoding(char utf8[], int len)
+/* {{{ string houdini_escape_html(string str [, bool $secure = true ])
+   Escapes an HTML string */
+PHP_FUNCTION(houdini_escape_html)
 {
-	size_t pos = 0;
-	int status;
+	char *string = NULL;
+	int length;
+	zend_bool secure = 1;
 
-	while (pos < len) {
-		php_houdini_next_utf8_char((const unsigned char *)utf8, len, &pos, &status);
-		if (status != SUCCESS) {
-			return 0;
-		}
+	gh_buf buffer = GH_BUF_INIT;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|b", &string, &length, &secure) == FAILURE) {
+		return;
 	}
-	return 1;
+
+	if (length == 0) {
+		RETURN_EMPTY_STRING();
+	}
+
+	PHP_HOUDINI_CHECK_UTF8(string, length);
+
+	if (houdini_escape_html0(&buffer, (const uint8_t *)string, length, secure)) {
+		RETVAL_STRINGL(buffer.ptr, buffer.size, 1);
+		gh_buf_free(&buffer);
+	} else {
+		RETVAL_STRINGL(string, length, 1);
+	}
 }
+/* }}} */
+
+/* {{{ proto string houdini_unescape_html(string str)
+   Unescapes an HTML string */
+PHP_FUNCTION(houdini_unescape_html) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_unescape_html);
+}
+/* }}} */
+
+/* {{{ proto string houdini_escape_js(string str)
+   Escapes a JavaScript string */
+PHP_FUNCTION(houdini_escape_js) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_escape_js);
+}
+/* }}} */
+
+/* {{{ proto string houdini_unescape_js(string str)
+   Unescapes a JavaScript string */
+PHP_FUNCTION(houdini_unescape_js) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_unescape_js);
+}
+/* }}} */
+
+/* {{{ proto string houdini_escape_uri(string str)
+   Escapes a URI string */
+PHP_FUNCTION(houdini_escape_uri) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_escape_uri);
+}
+/* }}} */
+
+/* {{{ proto string houdini_unescape_uri(string str)
+   Unescapes a URI string */
+PHP_FUNCTION(houdini_unescape_uri) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_unescape_uri);
+}
+/* }}} */
+
+/* {{{ proto string houdini_escape_url(string str)
+   Escapes a URL string */
+PHP_FUNCTION(houdini_escape_url) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_escape_url);
+}
+/* }}} */
+
+/* {{{ proto string houdini_unescape_url(string str)
+   Unescapes a URL string */
+PHP_FUNCTION(houdini_unescape_url) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_unescape_url);
+}
+/* }}} */
+
+/* {{{ proto string houdini_escape_xml(string str)
+   Escapes an XML string */
+PHP_FUNCTION(houdini_escape_xml) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_escape_xml);
+}
+/* }}} */
+
+/* {{{ proto string houdini_escape_href(string str)
+   Escapes an HREF string */
+PHP_FUNCTION(houdini_escape_href) \
+{
+	PHP_HOUDINI_FUNCTION(houdini_escape_href);
+}
+/* }}} */
 
 const zend_function_entry houdini_functions[] = {
-	PHP_FE(houdini_escape_html,		NULL)
-	PHP_FE(houdini_unescape_html,	NULL)
-	PHP_FE(houdini_escape_js,		NULL)
-	PHP_FE(houdini_unescape_js,		NULL)
-	PHP_FE(houdini_escape_uri,		NULL)
-	PHP_FE(houdini_unescape_uri,	NULL)
-	PHP_FE(houdini_escape_url,		NULL)
-	PHP_FE(houdini_unescape_url,	NULL)
-	PHP_FE(houdini_escape_xml,		NULL)
-	PHP_FE(houdini_escape_href,		NULL)
-	PHP_FE_END
+	PHP_FE(houdini_escape_html,      NULL)
+	PHP_FE(houdini_unescape_html,    NULL)
+	PHP_FE(houdini_escape_js,        NULL)
+	PHP_FE(houdini_unescape_js,      NULL)
+	PHP_FE(houdini_escape_uri,       NULL)
+	PHP_FE(houdini_unescape_uri,     NULL)
+	PHP_FE(houdini_escape_url,       NULL)
+	PHP_FE(houdini_unescape_url,     NULL)
+	PHP_FE(houdini_escape_xml,       NULL)
+	PHP_FE(houdini_escape_href,      NULL)
+	{ NULL, NULL, NULL }
 };
 
 PHP_MINIT_FUNCTION(houdini)
@@ -215,70 +306,6 @@ PHP_MINFO_FUNCTION(houdini)
 	php_info_print_table_row(2, "Version", PHP_HOUDINI_EXT_VERSION);
 	php_info_print_table_end();
 }
-
-/* proto string houdini_escape_html(string str [, bool $secure = true ])
-   Escapes an HTML string */
-PHP_FUNCTION(houdini_escape_html)
-{
-	char *string = NULL;
-	int length;
-	zend_bool secure = 1;
-
-	gh_buf buffer = GH_BUF_INIT;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|b", &string, &length, &secure) == FAILURE) {
-		return;
-	}
-
-	if (length == 0) {
-		RETURN_EMPTY_STRING();
-	}
-
-	PHP_HOUDINI_CHECK_UTF8(string, length)
-
-	if (houdini_escape_html0(&buffer, (const uint8_t *)string, length, secure)) {
-		RETVAL_STRINGL(buffer.ptr, buffer.size, 1);
-		gh_buf_free(&buffer);
-	} else {
-		RETVAL_STRINGL(string, length, 1);
-	}
-}
-
-/* proto string houdini_unescape_html(string str)
-   Unescapes an HTML string */
-PHP_HOUDINI_FUNCTION(houdini_unescape_html)
-
-/* proto string houdini_escape_js(string str)
-   Escapes a JavaScript string */
-PHP_HOUDINI_FUNCTION(houdini_escape_js)
-
-/* proto string houdini_unescape_js(string str)
-   Unescapes a JavaScript string */
-PHP_HOUDINI_FUNCTION(houdini_unescape_js)
-
-/* proto string houdini_escape_uri(string str)
-   Escapes a URI string */
-PHP_HOUDINI_FUNCTION(houdini_escape_uri)
-
-/* proto string houdini_unescape_uri(string str)
-   Unescapes a URI string */
-PHP_HOUDINI_FUNCTION(houdini_unescape_uri)
-
-/* proto string houdini_escape_url(string str)
-   Escapes a URL string */
-PHP_HOUDINI_FUNCTION(houdini_escape_url)
-
-/* proto string houdini_unescape_url(string str)
-   Unescapes a URL string */
-PHP_HOUDINI_FUNCTION(houdini_unescape_url)
-
-/* proto string houdini_escape_xml(string str)
-   Escapes an XML string */
-PHP_HOUDINI_FUNCTION(houdini_escape_xml)
-
-/* proto string houdini_escape_href(string str)
-   Escapes an HREF string */
-PHP_HOUDINI_FUNCTION(houdini_escape_href)
 
 zend_module_entry houdini_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
